bb4eca046fbd26386e35bdb1b81513d2f3a9447b
KailaTheeNgineer <kamcalpine717@gmail.com>
2024-05-15 11:01:49 -0400

    Deli home made

---
+++ src/main/java/pluralsight/GourmetDeli.java
@@ -0,0 +1,46 @@
+package pluralsight;
+
+import java.util.Scanner;
+
+public class GourmetDeli {
+
+    // creating static scanner to access at all times
+    public static Scanner myScanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+
+        // While loop for the home screen
+        boolean home = true;
+        while (home) {
+            System.out.println("Gourmet Deli Home");
+            System.out.println("1) New Order");
+            System.out.println("X) Exit");
+            String homeInput = myScanner.nextLine();
+
+            switch (homeInput) {
+                case "1":
+                    break;
+                case "x", "X":
+                    home = false;
+                    break;
+                default:
+                    System.out.println("Select a Valid Option");
+
+
+
+            }
+
+
+
+
+
+
+
+
+        }
+
+
+
+
+    }
+}
