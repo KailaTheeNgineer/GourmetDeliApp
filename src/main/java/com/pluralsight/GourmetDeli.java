5f4240a275e93cf602a9ef3f2f1e214462ddc61f
KailaTheeNgineer <kamcalpine717@gmail.com>
2024-05-26 11:03:57 -0400

    Adding new sandwiches

--- src/main/java/com/pluralsight/GourmetDeli.java
+++ src/main/java/com/pluralsight/GourmetDeli.java
@@ -29,66 +29,75 @@ public class GourmetDeli {
                     while (newOrder) {
                         System.out.println(WHITE + "\n\nCreate A New Order: ");
                         System.out.println("        1) Add Sandwich\n        2) Add Drink        \n        3) Add Chips\n        4) Checkout");
-                        System.out.println("0) Cancel Order/Back Home");
+                        System.out.println("0) Cancel Order");
                         String orderScreenInput = myScanner.nextLine();
 
                         switch  (orderScreenInput) {
 
                             case "1":
                                 // while loop to exit adding a sandwich
-                               boolean addSandwich = true;
+                                boolean addSandwich = true;
 
-                               while(addSandwich) {
-                                   System.out.println(WHITE + "Add A Sandwich\n\n\n");
-                                   System.out.println("Select a Sandwich Size\n1) 4in\n" +
-                                           "2) 8in\n3) 12in");
-                                   String userSizeSandwich = myScanner.nextLine();
-                                   System.out.println("Bread Type: \n1) White\n2) Wheat\n3) Rye\n4) Wrap");
-                                   String userBreadType = myScanner.nextLine();
-                                   System.out.println("Meat Type: \n1) Steak\n2) Ham\n3) Salami\n4) Roast Beef");
-                                   System.out.println("5) Chicken\n6) Bacon 7) None");
-                                   String userMeatType = myScanner.nextLine();
-                                   System.out.println("Would you like extra protein? Y/N (additional cost)");
-                                   String userExtraMeat = myScanner.nextLine();
-                                   System.out.println("Cheese Type: \n1) American\n2) Provolone\n3) Cheddar\n4) Swiss\n5) None");
-                                   String userCheeseType = myScanner.nextLine();
-                                   System.out.println("Would you like extra cheese? Y/N (additional cost)");
-                                   String userExtraCheese = myScanner.nextLine();
+                                while(addSandwich) {
+                                    System.out.println(WHITE + "Add A Sandwich\n\n\n");
+                                    System.out.println("Select a Sandwich Size\n1) 4in\n" +
+                                            "2) 8in\n3) 12in");
+                                    String userSizeSandwich = myScanner.nextLine();
+                                    System.out.println("Bread Type: \n1) White\n2) Wheat\n3) Rye\n4) Wrap");
+                                    String userBreadType = myScanner.nextLine();
+                                    System.out.println("Meat Type: \n1) Steak\n2) Ham\n3) Salami\n4) Roast Beef");
+                                    System.out.println("5) Chicken\n6) Bacon \n7) Continue");
+                                    String userMeatType = myScanner.nextLine();
+                                    System.out.println("Would you like extra protein? Y/N (additional cost)");
+                                    String userExtra1Meat = myScanner.nextLine();
+                                    boolean userExtraMeat = Boolean.getBoolean(userExtra1Meat);
+                                    System.out.println("Cheese Type: \n1) American\n2) Provolone\n3) Cheddar\n4) Swiss\n5) Continue");
+                                    String userCheeseType = myScanner.nextLine();
+                                    System.out.println("Would you like extra cheese? Y/N (additional cost)");
+                                    String user1ExtraCheese = myScanner.nextLine();
+                                    boolean userExtraCheese = Boolean.getBoolean(user1ExtraCheese);
 
-                                   boolean addToppings = true;
+                                    boolean addToppings = true;
 
-                                   while (addToppings) {
+                                    while (addToppings) {
 
-                                       System.out.println("Add Toppings: \n1) Lettuce 2) Peppers 3) Onions 4) Tomatoes\n"
-                                               + "5) Jalapenos 6) Cucumbers 7) Pickles 8) Guacamole 9) Mushrooms 0) Continue");
-                                       String userToppings = myScanner.nextLine();
+                                        System.out.println("Add Toppings: \n1) Lettuce 2) Peppers 3) Onions 4) Tomatoes\n"
+                                                + "5) Jalapenos 6) Cucumbers 7) Pickles 8) Guacamole 9) Mushrooms 0) Continue");
+                                        String userToppings = myScanner.nextLine();
 
-                                       if (userToppings.equals("0")) {
-                                           addToppings = false;
-                                       }
+                                        if (userToppings.equals("0")) {
+                                            addToppings = false;
+                                        }
+                                    }
                                         boolean addCondiments = true;
 
-                                       while (addCondiments) {
+                                        while (addCondiments) {
 
-                                           System.out.println("Add Sauce: \n1) Mayo 2) Mustard 3) Ketchup\n" +
-                                                   "4) Ranch 5) Vinaigrette 6) Thousand Islands 0) Continue");
-                                           String userCondiments = myScanner.nextLine();
+                                            System.out.println("Add Condiments: \n1) Mayo 2) Mustard 3) Ketchup\n" +
+                                                    "4) Ranch 5) Vinaigrette 6) Thousand Islands 0) Continue");
+                                            String userCondiments = myScanner.nextLine();
 
-                                           if (userCondiments.equals("0")) {
-                                               addCondiments = false;
-                                           }
+                                            if (userCondiments.equals("0")) {
+                                                addCondiments = false;
+                                            }
 
-                                       }
+                                        }
 
-                                       System.out.println("Would you like it toasted? (Y/N)");
-                                       String userToastSandwich = myScanner.nextLine();
-                                   }
-
-
+                                        System.out.println("Would you like it toasted? (Y/N)");
+                                        String userToast1Sandwich = myScanner.nextLine();
+                                        boolean userToastSandwich = Boolean.getBoolean(userToast1Sandwich);
 
 
+                                     //Parse the size sandwich into a string holding the size sandwich
+                                    // Sandwich userSandiwch = new SandwichBuilder(userSizeSandwich);
 
-                               }
+                                    }
+
+
+
+
+
+
                                 break;
                             case "2":
                                 break;
